cda83485da9166d0d8bf6ad8aeae7cab23cf45c1
ThedonJim <royouma50@gmail.com>
2026-01-06 11:51:53 +0300

    Add post content field and simplify Posts schema

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -1,24 +1,21 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema(
-  {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    title: { type: String },
-    desc: { type: String },
-    img: { type: String },
-    slug: { type: String },
-    cat: { type: String },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comments" }],
-    views: [{ type: mongoose.Schema.Types.ObjectId, ref: "Views" }],
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    status: { type: Boolean, default: false },
-    // moderation fields
-    approved: { type: Boolean, default: false },
-    approvedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
-    approvedAt: { type: Date, required: false },
-  },
-  { timestamps: true }
-);
+const postSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: String,
+  desc: String,
+  content: String, // blog body
+  img: String,
+  slug: String,
+  cat: String,
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comments" }],
+  views: [{ type: mongoose.Schema.Types.ObjectId, ref: "Views" }],
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  status: { type: Boolean, default: false },
+  approved: { type: Boolean, default: false },
+  approvedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  approvedAt: { type: Date },
+}, { timestamps: true });
 
 const Posts = mongoose.model("Posts", postSchema);
 export default Posts;
